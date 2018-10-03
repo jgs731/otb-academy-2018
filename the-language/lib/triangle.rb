a4995e06e918dfc0841a55fbfcf66e5dc142efc3
Joshua Smith <joshua.smith@onthebeach.co.uk>
2018-10-03 10:46:45 +0100

    triangle 2 spec complete - NEEDS REFACTORING

--- the-language/lib/triangle.rb
+++ the-language/lib/triangle.rb
@@ -1,4 +1,4 @@
-# Triangle Project Code.
+	# Triangle Project Code.
 
 # Triangle analyzes the lengths of the sides of a triangle
 # (represented by a, b and c) and returns the type of triangle.
@@ -14,7 +14,25 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if zero_dimensions?(a,b,c) || negative_dimensions?(a, b, c)
+
+  if (a == b) && (b == c) && (a == c)
+  	:equilateral
+  elsif ((a == b ) && (c < a)) || ((b == c) && (a < b)) || ((a == c) && (b < c))
+  	:isosceles
+  elsif (a != b) && (b != c) && (a != c)
+  	:scalene
+  else
+  	raise TriangleError
+  end
+end
+
+def zero_dimensions?(a, b, c)
+	a.zero? || b.zero? || c.zero?
+end
+
+def negative_dimensions?(a, b, c)
+	a.negative? || b.negative? || c.negative?
 end
 
 # Error class used in part 2.  No need to change this code.
--- the-language/spec/keyword_args_spec.rb
+++ the-language/spec/keyword_args_spec.rb
@@ -5,17 +5,10 @@ RSpec.describe "ruby's keyword args" do
   end
 
   it "is possible to define a method with keyword arguments" do
-<<<<<<< HEAD
     expect( method_with_keyword_arguments.class ).to eq( Array )
     expect( method_with_keyword_arguments ).to eq( [1, 'two'] )
     expect( method_with_keyword_arguments(one: 'one') ).to eq( ['one', 'two'] )
     expect( method_with_keyword_arguments(two: 2) ).to eq( [1, 2] )
-=======
-    expect( method_with_keyword_arguments.class ).to eq( __ )
-    expect( method_with_keyword_arguments ).to eq( __ )
-    expect( method_with_keyword_arguments(one: 'one') ).to eq( __ )
-    expect( method_with_keyword_arguments(two: 2) ).to eq( __ )
->>>>>>> 71bfb2df2a174ad9e2f25e09abadb8963298cd2b
   end
 
   def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
@@ -23,11 +16,7 @@ RSpec.describe "ruby's keyword args" do
   end
 
   it "is possible to define a method with regular and keyword arguments" do
-<<<<<<< HEAD
     expect { method_with_keyword_arguments_with_mandatory_argument }.to raise_error( ArgumentError, /given 0, expected 1/ )
-=======
-    expect { method_with_keyword_arguments_with_mandatory_argument }.to raise_error( __, /__/ )
->>>>>>> 71bfb2df2a174ad9e2f25e09abadb8963298cd2b
   end
 
 end
