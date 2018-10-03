b2c20075e8f986a5aa6792e84fe0c4aeb3e04831
Joshua Smith <joshua.smith@onthebeach.co.uk>
2018-10-03 09:45:21 +0100

    keyword args complete

---
+++ the-language/spec/keyword_args_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe "ruby's keyword args" do
+
+  def method_with_keyword_arguments(one: 1, two: 'two')
+    [one, two]
+  end
+
+  it "is possible to define a method with keyword arguments" do
+    expect( method_with_keyword_arguments.class ).to eq( Array )
+    expect( method_with_keyword_arguments ).to eq( [1, 'two'] )
+    expect( method_with_keyword_arguments(one: 'one') ).to eq( ['one', 'two'] )
+    expect( method_with_keyword_arguments(two: 2) ).to eq( [1, 2] )
+  end
+
+  def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
+    [one, two, three]
+  end
+
+  it "is possible to define a method with regular and keyword arguments" do
+    expect { method_with_keyword_arguments_with_mandatory_argument }.to raise_error( ArgumentError, /given 0, expected 1/ )
+  end
+
+end
