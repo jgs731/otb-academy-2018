ee492461cb8b61c33ba8e50223f878e72696adda
Joshua Smith <joshua.smith@onthebeach.co.uk>
2018-10-09 11:10:38 +0100

    refactored bottles mini project

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -6,29 +6,53 @@ class Bottles
   def verses(start, finish)
     start.downto(finish).map { |number| verse(number) }.join("\n")
   end
-
+    
   def verse(number)
-    case number
-    when 0
-      <<~VERSE
-        No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall.
-        VERSE
-    when 1
-      <<~VERSE
-        1 bottle of beer on the wall, 1 bottle of beer.
-        Take it down and pass it around, no more bottles of beer on the wall.
-        VERSE
-    when 2
-      <<~VERSE
-        2 bottles of beer on the wall, 2 bottles of beer.
-        Take one down and pass it around, 1 bottle of beer on the wall.
-        VERSE
+    <<~VERSE
+      #{remaining_bottles(number).capitalize} #{container(number)} of beer on the wall, #{remaining_bottles(number)} #{container(number)} of beer.
+      #{second_line(number)}, #{remaining_bottles(successor(number))} #{container(successor(number))} of beer on the wall.
+      VERSE
+  end
+    
+  private
+    
+  def container(number)
+    if number == 1 
+      'bottle'
     else
-      <<~VERSE
-        #{number} bottles of beer on the wall, #{number} bottles of beer.
-        Take one down and pass it around, #{number - 1} bottles of beer on the wall.
-        VERSE
+      'bottles'
+    end
+  end
+      
+  def lyric_change(number)
+    if number == 1
+      'it'
+    else
+      'one'
+    end
+  end
+    
+  def remaining_bottles(num)
+    if num == 0
+      'no more'
+    else
+      num.to_s
+    end
+  end
+    
+  def successor(num)
+    if num == 0
+      99
+    else 
+      num - 1
+    end
+  end
+
+  def second_line(num)
+    if num == 0
+      'Go to the store and buy some more'
+    else
+      "Take #{lyric_change(num)} down and pass it around"
     end
   end
 end
